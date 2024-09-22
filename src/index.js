1ba7ddee634d490fbabfd23fd6db7c3f8059d5d1
yahya-mohamoud <yahyamohamoud625@gmail.com.com>
2024-09-22 10:44:28 +0300

    committed and added the image

--- src/index.js
+++ src/index.js
@@ -1,1 +1,14 @@
-console.log("alert");
+import "./style.css"
+import img from "./image/img"
+
+const content = document.querySelector("#content");
+
+const title = document.createElement("h1")
+title.innerText = "Yahya's Cusine";
+
+const image = document.createElement("img")
+img.src = img;
+
+
+content.appendChild(title);
+content.appendChild(image)
