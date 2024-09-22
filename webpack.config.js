4aa0349b6cc05da2c865657526286a0ae4c3b129
yahya-mohamoud <yahyamohamoud625@gmail.com.com>
2024-09-22 10:05:52 +0300

    commited

--- webpack.config.js
+++ webpack.config.js
@@ -5,8 +5,8 @@ module.exports = {
     mode: "development",
     entry: "./src/index.js",
     output: {
-        path: path.resolve(__dirname, "dist"),
         filename: "main.js",
+        path: path.resolve(__dirname, "dist"),
         clean: true,
     },
 
