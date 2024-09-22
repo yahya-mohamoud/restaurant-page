fa2fba2a39cd95a326e87beb3781abca3cd197a6
yahya-mohamoud <yahyamohamoud625@gmail.com.com>
2024-09-22 09:53:10 +0300

    setting webserver

--- webpack.config.js
+++ webpack.config.js
@@ -9,5 +9,30 @@ module.exports = {
         clean: true,
     },
 
-    
+    devtool: "eval-source-map",
+    devServer: {
+        watchFiles: ["./src/restaurant.html"]
+    },
+
+    plugins: [
+        new HtmlWebpackPlugin({
+          template: "./src/restaurant.html",
+        }),
+      ],
+      module: {
+        rules: [
+          {
+            test: /\.css$/i,
+            use: ["style-loader", "css-loader"],
+          },
+          {
+            test: /\.html$/i,
+            loader: "html-loader",
+          },
+          {
+            test: /\.(png|svg|jpg|jpeg|gif)$/i,
+            type: "asset/resource",
+          },
+        ],
+      },    
 };
