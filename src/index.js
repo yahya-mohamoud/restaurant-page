605d723f50e2d9bcbbfd02f79d5f3cf115e02b73
yahya-mohamoud <yahyamohamoud625@gmail.com.com>
2024-09-25 09:00:45 +0300

    commiting the created files

---
+++ src/index.js
@@ -0,0 +1,5 @@
+import { sayHi } from "./menu";
+import { sayNigger } from "./menu";
+console.log(sayHi)
+console.log(sayNigger());
+
---
+++ src/menu.js
@@ -0,0 +1,4 @@
+export const sayHi = "helloo, index; iam fine and working";
+export const sayNigger = () => {
+    console.log("kne grow")
+}
