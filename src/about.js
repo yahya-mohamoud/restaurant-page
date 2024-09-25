1a3195da73bb3614a1da300b7bcbc9a6f57ccc0d
yahya-mohamoud <yahyamohamoud625@gmail.com.com>
2024-09-25 14:53:15 +0300

    to be continued

--- src/about.js
+++ src/about.js
@@ -1,6 +1,58 @@
-
+import "./about.css"
 
     const content = document.querySelector("#content")
     const aboutBtn = document.querySelector(".btnThree")
     
-  
+
+    export function renderAbout () {
+        const aboutTitle = document.createElement("div")
+        aboutTitle.classList.add("aboutTitle")
+
+        const aboutHeader = document.createElement("h1")
+        aboutHeader.classList.add("aboutUs")
+
+        aboutHeader.innerText = "About Us"
+        aboutTitle.appendChild(aboutHeader)
+
+        const paraOne = document.createElement("p")
+        paraOne.classList.add("paraOne")
+        paraOne.innerText = `Welcome to Dhaqansoor restaurant, where culinary excellence meets warm hospitality. Our story is one of passion, dedication, and a commitment to delivering an exceptional dining experience to our cherished guests.` 
+        aboutTitle.appendChild(paraOne)
+
+        content.appendChild(aboutTitle)
+
+        const journey = document.createElement("h2")
+        journey.classList.add("journey")
+        journey.innerText = "Our Journey"
+        aboutTitle.appendChild(journey)
+
+        const journeyPara = document.createElement("p")
+        journeyPara.classList.add("journeyPara")
+        journeyPara.innerText = `Founded in 2032, Dhaqansoor began as a dream to create a place where people could come together to enjoy delicious food in a welcoming atmosphere. Our founder, [Founder’s Name], envisioned a restaurant that would not only serve great meals but also become a cornerstone of the community.`
+        aboutTitle.appendChild(journeyPara)
+
+        const workHrs = document.createElement("h3")
+        workHrs.classList.add("workHrs")
+        workHrs.innerText = `Our Working Hours`
+        aboutTitle.appendChild(workHrs)
+
+        const hrsDiv = document.createElement("div")
+        hrsDiv.classList.add("hrsDiv")
+        hrsDiv.innerHTML = `
+                            <h4>Saturday to Thrusday </h4>
+                            <p> <i>8am morning to 10pm evening</i></p>
+                            <h4>Friday</h4>
+                            <p><i>6am to 12pm</i></p>`
+        aboutTitle.appendChild(hrsDiv)
+
+        const address = document.createElement("div")
+        address.classList.add("address")
+        address.innerHTML = `<h3>Our Address </h3>
+                              <address>
+                                visit Us at: <br>
+                                    wadada suuqa <br>
+                                    jidka 20ka, Jijiga <br>
+                                    Ethiopia
+                                </address>`
+       aboutTitle.appendChild(address)                         
+    }
--- src/index.js
+++ src/index.js
@@ -1,10 +1,18 @@
 import "./style.css"
 import { rendeMenu } from "./menu";
+import { renderAbout } from "./about";
 
+const content = document.querySelector("#content")
 const menuBtn = document.querySelector(".btnTwo")
-
-about()
+const aboutBtn = document.querySelector(".btnThree")
 
 menuBtn.addEventListener("click", () => {
+        content.innerHTML = ""
         rendeMenu()
+})
+
+aboutBtn.addEventListener("click", () => {
+        content.innerHTML = ""
+
+        renderAbout()
 })
